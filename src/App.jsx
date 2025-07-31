295606fe1d2016220d194bfa2acef41933449561
nmasters <nmasters52@gmail.com>
2025-07-31 16:20:41 -0400

    feat(api call) calls api, catches errors, sets loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+const API_URL = import.meta.env.VITE_COINS_API_URL;
 
 const App = () => {
+
+  const [coins, setCoins] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [limit, setLimit] = useState(10);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+        );
+        if(!res.ok) throw new Error('Failed to fetch data');
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err.message);
+      }
+    }
+    fetchCoins();
+  }, [limit])
+
   return (
-    <div>Crypto Dash 🚀</div>
+    <div>
+      <h1>Crypto Dash 🚀</h1>
+      {error}
+    </div>
   )
 }
 
