41ac5b4fcf30e978a6ee4918f938ce6b748f73ac
nmasters <nmasters52@gmail.com>
2025-08-16 07:23:45 -0400

    feat(routing & pages) add pages and routing
    - Using react-router in declarative modified
    - Moved rendering out of App.jsx and into HomePage.jsx
    - App.jsx holds the routes and passes state to child components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import CoinCard from "../components/CoinCard";
+import LimitControler from "../components/LimitControler";
+import CoinFilter from "../components/CoinFilter";
+import SortSelector from "../components/SortSelector";
+
+const HomePage = ({
+    coins,
+    filter,
+    setFilter,
+    limit,
+    setLimit,
+    sortBy,
+    setSortBy,
+    isLoading,
+    error
+}) => {
+
+const filteredCoins = 
+    coins.filter((coin) => {
+      return (
+        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(filter.toLowerCase())
+    );
+  })
+  .slice()
+  .sort((a,b) => {
+    switch (sortBy) {
+      case 'market_cap_desc' :
+        return b.market_cap - a.market_cap;
+      case 'market_cap_asc':
+        return a.market_cap - b.market_cap;
+      case 'price_desc':
+        return b.current_price - a.current_price;
+      case 'price_asc':
+        return a.current_price - b.current_price;
+      case 'change_desc':
+        return b.price_change_percentage_24h - a.price_change_percentage_24h;
+      case 'change_asc':
+        return a.price_change_percentage_24h - b.price_change_percentage_24h;
+    }
+  })
+
+  return (
+    <div>
+      <h1>🏃‍➡️ Crypto Dash 💰</h1>
+      {error && <p className='error'>{error}</p>}
+      {isLoading && <p>Loading...</p>}
+
+      <div className="top-controls">
+        <CoinFilter filter={filter} setFilter={setFilter} />
+        <SortSelector sortBy={sortBy} setSortBy={setSortBy} />
+        <LimitControler limit={limit} setLimit={setLimit} />
+      </div>
+      
+      { !isLoading && !error && (
+        <main className="grid">
+          {filteredCoins.length > 0 ? 
+          (filteredCoins.map((coin) =>  <CoinCard key={coin.id} coin={coin} />)) : 
+          (<p>No Coins Match</p>)}
+        </main>
+      )
+      }
+     
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const NotFoundPage = () => {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>404</h1>
+      <p style={styles.message}>OOps! The page you're looking for does not exist</p>
+      <Link to='/' style={styles.link}>⬅️ Go back home</Link>
+    </div>
+  )
+}
+
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    pading: '80px 20px',
+    color: '#fff'
+  },
+  title: {
+    fontSize: '72px',
+    marginBottom: '20px'
+  },
+  message: {
+    fontSize: '18px',
+    marginBottom: '30px'
+  },
+  link: {
+    textDecoration: 'none',
+    color: '#007bff',
+    fontWeight: 'bold'
+  }
+}
+
+export default NotFoundPage;
